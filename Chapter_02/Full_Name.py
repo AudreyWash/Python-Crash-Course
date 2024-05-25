2ef7f983c32a9eade4243988419325bb1a619e15
Audrey Wash <robinsonaudrey6106@gmail.com>
2024-05-25 07:45:57 -0500

    Edit Full_Name.py

--- Chapter_02/Full_Name.py
+++ Chapter_02/Full_Name.py
@@ -1,4 +1,13 @@
-first_name = "Audrey"
-last_name = "Washington"
+#Example 1
+first_name = "audrey"
+last_name = "washington"
 full_name = f"{first_name} {last_name}"
 print(f"Hello, {full_name.title()}!")
+
+
+#Example 2
+first_name2 = "audrey"
+last_name2 = "washington"
+full_name2 = f"{first_name2} {last_name2}"
+message = f"Hello, {full_name.title()}!"
+print(message)
