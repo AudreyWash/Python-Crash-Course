f20c4e2e24ba4bf1b62a2ddaf5912ea6f2e471ce
Audrey Wash <robinsonaudrey6106@gmail.com>
2024-05-25 09:13:07 -0500

    Add Message.py

---
+++ Chapter_02/Messages.py
@@ -0,0 +1,13 @@
+
+#Example 1
+name = "Audrey"
+print("Hello " + name + ", " + "would you like to learn some Python today?")
+
+#Example 2
+name2 = "Audrey"
+print("A" + name[1].upper() + "drey")
+print(name.lower())
+print(name.title())
+
+#Example 3
+
