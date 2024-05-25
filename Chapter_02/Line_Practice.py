4a4f29dc070670c4a2496537b2db9fddd6cd6edc
Audrey Wash <robinsonaudrey6106@gmail.com>
2024-05-25 08:35:29 -0500

    Add Line_Practice.py

---
+++ Chapter_02/Line_Practice.py
@@ -0,0 +1,12 @@
+#Example 1
+print("Languages:\n\tPython\n\tC\n\tJavaScript")
+
+print('\n')
+
+#Example 2
+favorite_language = ' python       '
+print(favorite_language)
+
+print(favorite_language.rstrip())
+print(favorite_language.lstrip())
+print(favorite_language.strip())
